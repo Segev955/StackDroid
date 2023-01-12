8ae53b302af99523e264bdfa7dc362c43b50d78b
Segev955 <segev95@gmail.com>
2023-01-12 09:05:49 +0200

    final

--- jsonchanger.py
+++ jsonchanger.py
@@ -7,40 +7,56 @@ def loud_from_json(filename):
         # Reading from json file
         return json.load(openfile)
 
+
 def save_to_json(filename, l):
     with open(filename, 'w') as outfile:
         json.dump(l, outfile)
 
+
 def replace_numbers(dict):
-    l=[]
+    l = []
     for n in dict:
-        if n ==0:
+        if n == 0:
             l.append(1)
-        elif n ==1:
+        elif n == 1:
             l.append(0)
         else:
             l.append(n)
     return l
 
+
 def random_numbers(dict):
     l = []
     for n in dict:
-        l.append(int(random()*2))
+        l.append(int(random() * 2))
     return l
+
 
 def run_replace(filename):
     save_to_json(filename, replace_numbers(loud_from_json(filename)))
 
+
+def get_list_replace(filename):
+    return replace_numbers(loud_from_json(filename))
+
+
 def run_replace_to_other_file(src, dest):
     save_to_json(dest, replace_numbers(loud_from_json(src)))
+
 
 def run_random(filename):
     save_to_json(filename, random_numbers(loud_from_json(filename)))
 
+
+def get_list_random(filename):
+    return random_numbers(loud_from_json(filename))
+
+
 def run_random_to_other_file(src, dest):
     save_to_json(dest, random_numbers(loud_from_json(src)))
 
+
 if __name__ == '__main__':
     filename = 'y_test.json'
-    run_random(filename)
-
+    print(get_list_random(filename))
+    print(get_list_replace(filename))
--- makecsv.py
+++ makecsv.py
@@ -4,38 +4,43 @@ import os
 import random
 import sys
 import hashlib
+
 # Define folder where the log files are located
 source_folder = 'drebin/samples/'
 dest_folder = 'drebin'
 
+
 # List of all the files
 # dataset = [x for x in os.listdir(source_folder) if x.endswith(".apk")]
 def rnd_mal_ben():
-    if(random.random()>0.5):
+    if (random.random() > 0.5):
         return "mal"
     return "ben"
 
-def makeCsv(csvname, folder = dest_folder):
-    l=createList()
-    f = open(f'{folder}/{csvname}', 'w',newline='')
+
+def makeCsv(csvname, folder=dest_folder):
+    l = createList()
+    f = open(f'{folder}/{csvname}', 'w', newline='')
     writer = csv.writer(f)
     print(l)
     writer.writerows(l)
     print(writer)
     f.close()
 
+
 def createList():
     l = []
     l.append(['apps', 'family'])
-    datamal= [x for x in os.listdir("drebin/mal")]
+    datamal = [x for x in os.listdir("drebin/mal")]
     databen = [x for x in os.listdir("drebin/ben")]
     for x in datamal:
-        l.append([x,1])
+        l.append([x, 1])
     for x in databen:
-        l.append([x,0])
+        l.append([x, 0])
     return l
 
 
 if __name__ == '__main__':
     makeCsv('sha256_family.csv')
     print(f"csvsaved.")
+
